78bc28c5b8d06abd1639c73af640c5ee5fd8276b
Magnus Revheim Martinsen <magnusrm.stud.ntnu.no>
2019-06-30 21:03:59 +0200

    Merge branches 'Christian' and 'magnus' of https://github.com/Magnusrm/EnviroHabit_k into magnus
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/e/envirohabit_k/RegisterActivity.kt
    #	app/src/main/res/layout/activity_register.xml

--- app/src/main/java/com/e/envirohabit_k/RegisterActivity.kt
+++ app/src/main/java/com/e/envirohabit_k/RegisterActivity.kt
@@ -3,42 +3,72 @@ package com.e.envirohabit_k
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.EditText
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
+
+    lateinit var authInstance: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+
+
         register_button.setOnClickListener {
-            registerUser()
-            logIn()
+            val username = findViewById<View>(R.id.username_input) as EditText
+            val email = findViewById<View>(R.id.email_input) as EditText
+            val password = findViewById<View>(R.id.password_input) as EditText
+
+
+            registerUser(email.text.toString(), password.text.toString())
+
+            saveData(username.text.toString())
         }
     }
 
-    private fun registerUser() {
-        val email = email_input.text.toString()
-        val password = password_input.toString()
-        val authInstance = FirebaseAuth.getInstance()
+    private fun registerUser(email: String, password: String) {
+        authInstance = FirebaseAuth.getInstance()
 
         authInstance.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener {
                 if (!it.isSuccessful) return@addOnCompleteListener
 
                 Log.d("Main", "successfully created user with uid: $email")
-                val uid = authInstance.currentUser?.uid.toString()
 
-                val user = hashMapOf(
-                    "uid" to uid,
-                    "email" to email,
-                    "username" to username_input.text.toString(),
-                    "points" to 0
-                )
+            }
+            .addOnFailureListener {
+                Log.d("Main", it.message.toString())
+            }
 
-                FirebaseFirestore.getInstance().collection("users").document(uid)
+
+    }
+        private fun saveData(username: String) {
+
+            val db = FirebaseFirestore.getInstance()
+            val settings = FirebaseFirestoreSettings.Builder()
+                .setPersistenceEnabled(true)
+                .build()
+            db.firestoreSettings = settings
+
+            val uid = authInstance.currentUser?.uid.toString()
+
+            val user = hashMapOf(
+                "uid" to uid,
+                "username" to username,
+                "points" to 0
+            )
+
+            Log.d("Main", "hehheh")
+            Log.d("Main", user.toString())
+
+                db.collection("users").document(uid)
                     .set(user)
                     .addOnSuccessListener {
                         Log.d("Main", "FUNKEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
@@ -49,20 +79,5 @@ class RegisterActivity : AppCompatActivity() {
                     }
 
 
-            }
-            .addOnFailureListener {
-                Log.d("Main", it.message.toString())
-            }
-
-
-
-    }
-
-    private fun logIn() {
-
-    }
-
-    private fun createUserData(uid : String) {
-
-    }
+        }
 }
