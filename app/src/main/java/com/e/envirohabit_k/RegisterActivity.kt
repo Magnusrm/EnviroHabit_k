0fd4072b49e0c26edb75df876fd0134e37715266
Magnus Revheim Martinsen <magnusrm.stud.ntnu.no>
2019-06-30 18:00:32 +0200

    createdbracng

--- app/src/main/java/com/e/envirohabit_k/RegisterActivity.kt
+++ app/src/main/java/com/e/envirohabit_k/RegisterActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
@@ -14,22 +15,54 @@ class RegisterActivity : AppCompatActivity() {
 
         register_button.setOnClickListener {
             registerUser()
+            logIn()
         }
     }
 
     private fun registerUser() {
         val email = email_input.text.toString()
         val password = password_input.toString()
+        val authInstance = FirebaseAuth.getInstance()
 
-        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+        authInstance.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener {
                 if (!it.isSuccessful) return@addOnCompleteListener
 
                 Log.d("Main", "successfully created user with uid: $email")
+                val uid = authInstance.currentUser?.uid.toString()
+
+                val user = hashMapOf(
+                    "uid" to uid,
+                    "email" to email,
+                    "username" to username_input.text.toString(),
+                    "points" to 0
+                )
+
+                FirebaseFirestore.getInstance().collection("users").document(uid)
+                    .set(user)
+                    .addOnSuccessListener {
+                        Log.d("Main", "FUNKEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
+
+                    }
+                    .addOnFailureListener {
+                        Log.d("Main", "Funk ikkje")
+                    }
+
+
             }
             .addOnFailureListener {
                 Log.d("Main", it.message.toString())
             }
+
+
+
+    }
+
+    private fun logIn() {
+
+    }
+
+    private fun createUserData(uid : String) {
 
     }
 }
---
+++ app/src/main/java/com/e/envirohabit_k/User.kt
@@ -0,0 +1,8 @@
+package com.e.envirohabit_k
+
+class User (uid : String, email : String, username : String, points : Number) {
+    val uid : String = uid
+    val email : String = email
+    val username : String = username
+    val points : Number = points
+}
