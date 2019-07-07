ee2be5436647d210021928c3f01721824143a3d0
christian <christian.foleide@gmail.com>
2019-07-07 21:12:33 +0200

    removed image from navbar

--- app/src/main/java/com/e/envirohabit_k/User.kt
+++
@@ -1,8 +0,0 @@
-package com.e.envirohabit_k
-
-class User (uid : String, email : String, username : String, points : Number) {
-    val uid : String = uid
-    val email : String = email
-    val username : String = username
-    val points : Number = points
-}
